c625a96df321da324acb4f88ddf9859d6ad2f20b
Josh <critesjosh@gmail.com>
2017-10-20 14:18:17 +0400

    lendermain.js, loan status watcher

---
+++ src/LenderMain.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+const Table  = require('./pure-components/table');
+
+const getLenderMainColumns = () => {
+	return [
+
+	]
+}
+
+class LenderMain extends Component {
+
+  render() {
+    return (
+      <div>
+      <p>LenderMain</p>
+      
+      </div>
+    );
+  }
+}
+
+export default LenderMain
