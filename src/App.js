559202f7194557becf537e9adfacc0869a1cf1c5
Josh <critesjosh@gmail.com>
2017-10-20 13:17:04 +0400

    watchers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import LoanFactory from '../build/contracts/LoanFactory.json'
+import Loan from '../build/contracts/Loan.json'
 import getWeb3 from './utils/getWeb3'
 import Home from './Home'
 import BorrowerMain from './BorrowerMain'
@@ -17,6 +18,8 @@ import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
+
+const contract = require('truffle-contract')
 
 class App extends Component {
   constructor(props) {
@@ -55,9 +58,12 @@ class App extends Component {
   }
 
   instantiateContract() {
-    const contract = require('truffle-contract')
+    //const contract = require('truffle-contract')
     const loanFactory = contract(LoanFactory)
-    loanFactory.setProvider(this.state.web3.currentProvider)
+    loanFactory.setProvider(this.appContext.web3.currentProvider)
+    const loanContract = contract(Loan)
+    loanContract.setProvider(this.appContext.web3.currentProvider)
+    this.appContext.loanContract = loanContract
 
     this.state.web3.eth.getAccounts((error, accounts) => {
       loanFactory.deployed().then((instance) => {
@@ -86,6 +92,7 @@ class App extends Component {
               const auditor = result.args.auditor
               const amount = result.args.amount
               const IPFShash = result.args.IPFShash
+              const address = result.args.loan
 
               var loan = {}
               loan.lender = lender
@@ -93,12 +100,53 @@ class App extends Component {
               loan.auditor = auditor
               loan.amount = amount
               loan.IPFShash = IPFShash
+              loan.address = address
+              loan.isDefaulted = false
+              loan.isRepayed = false
 
               var loans = this.state.loans;
               loans.push(loan)
               this.setState({
                   loans: loans
               })
+
+              this.watchForDefaults(address)
+              this.watchForRepayment(address)
+          }
+      })
+  }
+
+  watchForDefaults(loanAddress){
+      const loanInstance = this.appContext.loanContract.at(loanAddress)
+      loanInstance.LogLoanInDefault({}, {fromBlock: 0})
+      .watch((err, result) => {
+          if(err) {
+              console.log(err)
+              return
+          } else {
+              console.log(result)
+              const isDefaulted = result.args.isDefaulted
+
+              var loans = this.state.loans
+              var curLoan = _.find(loans, { address: loanAddress })
+              var loan = loans.slice(loans.address)
+          }
+      })
+  }
+
+  watchForRepayment(loanAddress){
+      const loanInstance = this.appContext.loanContract.at(loanAddress)
+      loanInstance.LogLoanRepayed({}, {fromBlock: 0})
+      .watch((err, result) => {
+          if(err) {
+              console.log(err)
+              return
+          } else {
+              console.log(result)
+              const isRepayed = result.args.isRepayed
+
+              var loans = this.state.loans
+              var loan = loans.slice()
           }
       })
   }
@@ -122,6 +170,7 @@ class App extends Component {
             <Route exact path="/borrower" render={() => <BorrowerMain currentState={this.state} functions={functions} />}/>
             <Route exact path="/borrower/:address" render={() => <BorrowerLoanDetails currentState={this.state} functions={functions} />}/>
             <Route exact path="/auditor" render={() => <AuditorMain currentState={this.state} functions={functions} />}/>
+
           </Switch>
         </Router>
       </div>
