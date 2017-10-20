2ed2840fa0a230d7e37c7b608bb841dd770c9a20
Jandrews <jordandrews@hotmail.com>
2017-10-20 14:12:37 +0400

    "work work work work work" - Rihanna 2017

--- src/BorrowerLoanDetails.js
+++ src/BorrowerLoanDetails.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
 class BorrowerLoanDetails extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      payMerchantAmount: 0
+    };
+  }
+
+  onChange(e){
+    this.setState({
+      payMerchantAmount: e.target.value
+    })
+  }
+
+  payMerchant(){
+    // Pay merchant
+    console.log('pay merchant');
+  }
 
   render() {
     return (
       <div>
-      <p>Borrower Loan Details</p>
+        <h2>Loan Details<h2>
+        <BorrowerLoanRepayment />
+        {/* Can move into own file if necessary */}
+        <div>
+          <select>
+            <option value="merchantAddress">merchantAddress placeholder</option>
+          </select>
+          <input type="number" value={this.state.payMerchantAmount} onChange={(e) => this.onChange(e)}></input>
+          <button type="button" onClick={() => this.payMerchant()}>Pay merchant</button>
+        </div>
+
       </div>
     );
   }
